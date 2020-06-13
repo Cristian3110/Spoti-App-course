8a2503a4e0c5f6325e107151d79f2e2becef937d
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-06-13 02:58:00 -0400

    creación del search y conexión con servicio

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,7 +13,7 @@ export class SpotifyService {
     const headers = new HttpHeaders({
       // aqui voy a  especificar todos los token de la API
       Authorization:
-        'Bearer BQD9ByBesFeJ3t_ys62SwrwqbPzfaxHcdPV9LI3GO8NSJAdaDg2PuvKGlZsyBiGcyaIRFl0Rvoh4ib-dqB4',
+        'Bearer BQCHAqlQZde6BT6ew_eAzTuHZkqV7X4QXxNfqFsOlfDm5ezjv1asKb9kvp8Orp3xYp-46RK-RivYTfeG-zQ',
     });
 
     return this.http.get(
@@ -25,5 +25,19 @@ export class SpotifyService {
     // .subscribe((data) => {
     //   console.log(data);
     // });
+  }
+  getArtista(termino: string) {
+    const headers = new HttpHeaders({
+      // aqui voy a  especificar todos los token de la API
+      Authorization:
+        'Bearer BQCHAqlQZde6BT6ew_eAzTuHZkqV7X4QXxNfqFsOlfDm5ezjv1asKb9kvp8Orp3xYp-46RK-RivYTfeG-zQ',
+    });
+
+    return this.http.get(
+      `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,
+      {
+        headers,
+      }
+    );
   }
 }
