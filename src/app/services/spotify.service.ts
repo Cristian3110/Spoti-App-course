735a94695cdcc323d2daf1d3a09757bc8a0722ed
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-06-15 23:16:00 -0400

    optimización y centralizando las peticiones hacia spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,40 +12,53 @@ export class SpotifyService {
     console.log('spotify service listo');
   }
 
-  getNewRelease() {
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+
     const headers = new HttpHeaders({
       // aqui voy a  especificar todos los token de la API
       Authorization:
-        'Bearer BQBxVlqBlclH1hgKNitFvLFfaV4WG7VqV-TZXgwtPRpmInmFg6gj9BBTygt1tcBcDZoVXnslz9RHJlGiQEw',
+        'Bearer BQBH9qTOEMeJ0wWQDujeDD2BM6pFUfNRwE4GY0cP6WSL2bBGYjpcYBtY76yfImEkO-vyUgvHhC-LYaXpvM0',
     });
 
-    return this.http
-      .get('https://api.spotify.com/v1/browse/new-releases?limit=20', {
-        headers,
-      })
-      .pipe(map((data) => data['albums'].items));
+    return this.http.get(url, { headers });
+  }
+
+  getNewRelease() {
+    // const headers = new HttpHeaders({
+    //   // aqui voy a  especificar todos los token de la API
+    //   Authorization:
+    //     'Bearer BQBxVlqBlclH1hgKNitFvLFfaV4WG7VqV-TZXgwtPRpmInmFg6gj9BBTygt1tcBcDZoVXnslz9RHJlGiQEw',
+    // });
+
+    return this.getQuery('browse/new-releases?limit=20').pipe(
+      map((data) => data['albums'].items)
+    );
+
+    // this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {
+    //   headers,
+    // });
     // .subscribe((data) => {
     //   console.log(data);
     // });
   }
   getArtista(termino: string) {
-    const headers = new HttpHeaders({
-      // aqui voy a  especificar todos los token de la API
-      Authorization:
-        'Bearer BQBxVlqBlclH1hgKNitFvLFfaV4WG7VqV-TZXgwtPRpmInmFg6gj9BBTygt1tcBcDZoVXnslz9RHJlGiQEw',
-    });
+    // const headers = new HttpHeaders({
+    //   // aqui voy a  especificar todos los token de la API
+    //   Authorization:
+    //     'Bearer BQBxVlqBlclH1hgKNitFvLFfaV4WG7VqV-TZXgwtPRpmInmFg6gj9BBTygt1tcBcDZoVXnslz9RHJlGiQEw',
+    // });
 
-    return this.http
-      .get(
-        `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,
-        {
-          headers,
-        }
-      )
-      .pipe(
-        map((data) => {
-          return data['artists'].items;
-        })
-      );
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`).pipe(
+      map((data) => data['artists'].items)
+    );
   }
+
+  // this.http
+  //   .get(
+  //     `https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,
+  //     {
+  //       headers,
+  //     }
+  //   )
 }
