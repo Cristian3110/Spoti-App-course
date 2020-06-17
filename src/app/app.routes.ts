fd19217173e642d0528ee2cce100a64d4fefcb5a
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-06-17 01:25:00 -0400

    Creando la nueva ruta del artista con paramero url y servicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
 
 // RUTAS A UTILIZAR EN LA APLICACIÓN
 
 export const ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
+  { path: 'artist/:id', component: ArtistaComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
