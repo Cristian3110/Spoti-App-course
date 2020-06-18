9a0738fe2442a3930cf5c6e3c8f30b99686b3688
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-06-18 14:44:25 -0400

    realizando mensaje de error en el search component, finalizado. Actualización

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -9,15 +9,27 @@ export class SearchComponent {
   artistas: any[] = [];
   loading: boolean;
 
+  error: boolean;
+  mensajeError: string;
+
   constructor(private spotify: SpotifyService) {}
 
   buscar(termino: string) {
     console.log(termino);
     this.loading = true;
-    this.spotify.getArtistas(termino).subscribe((data: any) => {
-      console.log(data);
-      this.artistas = data;
-      this.loading = false;
-    });
+
+    this.spotify.getArtistas(termino).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.artistas = data;
+        this.loading = false;
+      },
+      (errorServicio) => {
+        this.loading = false;
+        this.error = true;
+        console.log(errorServicio);
+        this.mensajeError = errorServicio.error.error.message;
+      }
+    );
   }
 }
